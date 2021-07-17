3480ee303212e08c542df556862b822e844b1aed
Syahrizal Fauzi <syahrizalfauzi16@gmail.com>
2021-07-17 13:42:55 +0700

    Matched daftar_warga response schema to client spec, added utils

--- app/handler.js
+++ app/handler.js
@@ -1,77 +1,70 @@
 const {wargaModel, iuranModel} = require('./models');
 const {Mongoose} = require('../config/database');
+const {convertDocToData} = require('./utils');
 
 const getAllWargaHandler = async (request, h) => {
-  const {sort, range} = request.query;
+  try {
+    const {sort, range} = request.query;
 
-  const sortParsed =
-    sort !== undefined
-      ? sort.replace(/[\[\]\'\"']+/g, '').split(',')
-      : undefined;
-  const sortParameter = sortParsed !== undefined ? sortParsed[0] : undefined;
-  const sortType = sortParsed !== undefined && sortParsed[1] == 'DESC' ? -1 : 1;
+    // Untuk header
+    const count = await wargaModel.countDocuments({});
 
-  const rangeParsed =
-    range !== undefined ? range.replace(/[\[\]']+/g, '').split(',') : undefined;
-  const min = rangeParsed !== undefined ? parseInt(rangeParsed[0]) : undefined;
-  const max = rangeParsed !== undefined ? parseInt(rangeParsed[1]) : undefined;
+    const sortParsed =
+      sort !== undefined
+        ? sort.replace(/[\[\]\'\"']+/g, '').split(',')
+        : undefined;
+    const sortParameter = sortParsed !== undefined ? sortParsed[0] : undefined;
+    const sortType =
+      sortParsed !== undefined && sortParsed[1] == 'DESC' ? -1 : 1;
 
-  const result =
-    sort !== undefined && range !== undefined
-      ? await wargaModel
-          .find()
-          .sort([[sortParameter, sortType]])
-          .skip(min)
-          .limit(max - min + 1)
-      : await wargaModel.find({});
+    const rangeParsed =
+      range !== undefined
+        ? range.replace(/[\[\]']+/g, '').split(',')
+        : undefined;
+    const min =
+      rangeParsed !== undefined ? parseInt(rangeParsed[0]) : undefined;
+    const max =
+      rangeParsed !== undefined ? parseInt(rangeParsed[1]) : undefined;
 
-  if (result.length !== 0) {
+    const result =
+      sort !== undefined && range !== undefined
+        ? await wargaModel
+            .find()
+            .sort([[sortParameter, sortType]])
+            .skip(min)
+            .limit(max - min + 1)
+        : await wargaModel.find({});
+
+    const data = result.map(convertDocToData);
+
     return h
-      .response({
-        status: 'success',
-        message: 'Daftar warga berhasil ditemukan',
-        docs: result,
-        items: {
-          total: result.length,
-          sort: sort,
-          range: range,
-        },
-      })
+      .response(data)
       .code(200)
       .header('Access-Control-Expose-Headers', 'X-Total-Count')
-      .header('X-Total-Count', result.length);
+      .header('X-Total-Count', count);
+  } catch (e) {
+    return h
+      .response({
+        status: 'fail',
+        message: 'Terjadi kesalahan pada server',
+      })
+      .code(500);
   }
-  // else if (result.length === 0) {
-  //   return h
-  //     .response({
-  //       status: 'fail',
-  //       message: 'Daftar warga tidak ditemukan',
-  //     })
-  //     .code(404);
-  // }
-  return h
-    .response({
-      status: 'fail',
-      message: 'Terjadi kesalahan pada server',
-    })
-    .code(500);
 };
 
 const addWargaHandler = async (request, h) => {
   try {
     const warga = new wargaModel(request.payload);
     const result = await warga.save();
+
+    const data = convertDocToData(result);
+
     return h
-      .response({
-        status: 'success',
-        message: 'Data warga berhasil ditambahkan',
-        docs: result,
-        items: {total: result.length},
-      })
+      .response(data)
       .code(201)
       .header('Access-Control-Expose-Headers', 'X-Total-Count')
       .header('X-Total-Count', result.length);
-  } catch {
+  } catch (e) {
     return h
       .response({
         status: 'fail',
@@ -82,91 +75,108 @@ const addWargaHandler = async (request, h) => {
 };
 
 const getWargaByIdHandler = async (request, h) => {
-  const {id} = request.params;
-  const result = await wargaModel
-    .find({_id: new Mongoose.Types.ObjectId(id)})
-    .exec();
-  if (result.length !== 0) {
+  try {
+    const {id} = request.params;
+    const result = await wargaModel
+      .find({_id: new Mongoose.Types.ObjectId(id)})
+      .exec();
+    if (result.length !== 0) {
+      const data = convertDocToData(result[0]);
+
+      return h
+        .response(data)
+        .code(200)
+        .header('Access-Control-Expose-Headers', 'X-Total-Count')
+        .header('X-Total-Count', result.length);
+    }
+
     return h
       .response({
-        status: 'success',
-        message: 'Data warga berhasil ditemukan',
-        docs: result,
-        items: {total: result.length},
+        status: 'fail',
+        message: 'Data warga tidak ditemukan',
       })
-      .code(200)
-      .header('Access-Control-Expose-Headers', 'X-Total-Count')
-      .header('X-Total-Count', result.length);
+      .code(404);
+  } catch (e) {
+    return h
+      .response({
+        status: 'fail',
+        message: 'Terjadi kesalahan pada server',
+      })
+      .code(500);
   }
-  return h
-    .response({
-      status: 'fail',
-      message: 'Data warga tidak ditemukan',
-    })
-    .code(404);
 };
 
 const updateWargaByIdHandler = async (request, h) => {
-  const {id} = request.params;
-  const result = await wargaModel.findOneAndUpdate(
-    {_id: new Mongoose.Types.ObjectId(id)},
-    request.payload,
-    {new: true, useFindAndModify: false}
-  );
-  if (result !== null) {
-    return h
-      .response({
-        status: 'success',
-        message: 'Data warga berhasil diperbarui',
-        docs: result,
-        items: {total: result.length},
-      })
-      .code(200)
-      .header('Access-Control-Expose-Headers', 'X-Total-Count')
-      .header('X-Total-Count', result.length);
-  } else if (result === null) {
+  try {
+    const {id} = request.params;
+
+    const result = await wargaModel.findOneAndUpdate(
+      {_id: new Mongoose.Types.ObjectId(id)},
+      request.payload,
+      {new: true, useFindAndModify: false}
+    );
+    if (result !== null) {
+      const data = convertDocToData(result);
+
+      return h
+        .response(data)
+        .code(200)
+        .header('Access-Control-Expose-Headers', 'X-Total-Count')
+        .header('X-Total-Count', result.length);
+    }
+
     return h
       .response({
         status: 'fail',
         message: 'Data tidak ditemukan',
       })
       .code(404);
+  } catch (e) {
+    return h
+      .response({
+        status: 'fail',
+        message: 'Terjadi kesalahan pada server',
+      })
+      .code(500);
   }
-  return h
-    .response({
-      status: 'fail',
-      message: 'Terjadi kesalahan pada server',
-    })
-    .code(500);
 };
 
 const deleteWargaByIdHandler = async (request, h) => {
-  const {id} = request.params;
-  const result = await wargaModel.deleteOne({
-    _id: new Mongoose.Types.ObjectId(id),
-  });
-  if (result.deletedCount !== 0) {
-    return h
-      .response({
-        status: 'success',
-        message: 'Data berhasil dihapus',
-      })
-      .code(200);
-  } else if (result.deletedCount === 0) {
+  try {
+    const {id} = request.params;
+
+    // Ambil docnya untuk response, di admin panel bisa Undo jadi perlu docnya
+    const doc = await wargaModel
+      .find({_id: new Mongoose.Types.ObjectId(id)})
+      .exec();
+
+    const result = await wargaModel.deleteOne({
+      _id: new Mongoose.Types.ObjectId(id),
+    });
+
+    if (result.deletedCount !== 0) {
+      const data = convertDocToData(doc[0]);
+      return h.response(data).code(200);
+    }
+
     return h
       .response({
         status: 'fail',
         message: 'Data tidak ditemukan',
       })
       .code(404);
+  } catch (e) {
+    return h
+      .response({
+        status: 'fail',
+        message: 'Terjadi kesalahan pada server',
+        error: e,
+      })
+      .code(500);
   }
-  return h
-    .response({
-      status: 'fail',
-      message: 'Terjadi kesalahan pada server',
-    })
-    .code(500);
 };
+
+//=============================================================//
 
 const getAllIuranHandler = async (request, h) => {
   const result = await iuranModel.find({});
---
+++ app/utils.js
@@ -0,0 +1,17 @@
+// By Ijal
+
+/**
+ * Dipake untuk convert doc yang diambil dari Mongoose ke data untuk response
+ * @param doc - Single document dari fetch database
+ * @return data - Data yang bisa langsung dioper untuk response
+ */
+const convertDocToData = (doc) => {
+  const id = doc._id;
+  delete doc._doc._id;
+  delete doc._doc.__v;
+  const data = {...doc._doc, id};
+
+  return data;
+};
+
+module.exports = {convertDocToData};
