1596e30bb96f88885e19f4925d0bb7a50add1145
firstaadi-dev <firstaadi@hotmail.com>
2021-07-21 15:49:24 +0700

    change ensureIndex to createIndexes on Mongoose

--- config/database.js
+++ config/database.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 Mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 const db = Mongoose.connection;
